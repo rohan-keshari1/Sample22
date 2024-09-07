d7b2cb8a2aa25d9fd1784c169abe801e00d65702
Rohan_Keshari <kesharirohan123@gmail.com>
2024-09-08 01:06:37 +0530

    This updated is for creating new branch RK1 and switching to new branch

--- mybranch.sh
+++ mybranch.sh
@@ -1,2 +1,5 @@
 This script file is for the understanding of GIT Branching with Merge and Rebase
 Author: RK
+
+Now we have created the new brach called RK1 and switch to RK1 branch and updating this file.
+
