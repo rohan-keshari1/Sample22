a84919c4fecaf07d2f2149c156dd28d2e2a15766
Rohan_Keshari <kesharirohan123@gmail.com>
2024-09-09 00:59:34 +0530

    removing the conflicts while rebasing cdrebase

--- test.sh
+++ test.sh
@@ -3,6 +3,10 @@ DevOps
 
 Added new branch called Anisble
 
+
 We have created this new branch called cdrebase to check the concept of git rebase
+
+Now we are going to merge the mergeexample branch to master ie cimerge
+a800861 (this changes was made to start merging cimerge to master branch)
 
 
